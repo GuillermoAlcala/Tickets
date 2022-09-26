fb470e8c88a9839e345c79b85b0f727508f4e64c
gchacon <alcalamemo@gmail.com>
2022-09-26 17:36:33 -0500

    Probando Toggle: se activa un text cuando esta prendido y en caso contrario se activa otro si esta apagado.

--- Tickets/Views/TipsView.swift
+++ Tickets/Views/TipsView.swift
@@ -11,30 +11,48 @@ struct TipsView: View {
     @State var Total:Int
     @State var CantidadPersonas:String
     @State private var  stepper: Int = 0
+    @State private var showingWelcome = false
+    
     var body: some View {
         NavigationView{
             ScrollView{
                 LazyVStack(alignment: .leading){
                     Text("El total es: \(Total)")
-                //    TextField("Cantidad de personas", text: ($CantidadPersonas))
-                //        .keyboardType(.numberPad)
-                //    Stepper("Cantidad de personas: \(stepper)", value: $stepper)
-                   
+                    //    TextField("Cantidad de personas", text: ($CantidadPersonas))
+                    //        .keyboardType(.numberPad)
+                    //    Stepper("Cantidad de personas: \(stepper)", value: $stepper)
+                    
+                }.padding()
+                
+                VStack {
+                    Toggle("Toggle label", isOn: $showingWelcome.animation(.spring()))
+                    
+                    if showingWelcome==true {
+                        Text("Toggle prendido")
+                            .font(.title)
+                            .foregroundColor(.yellow)
+
+                        }else{
+                        Text("Toggle apagado")
+                                .font(.title)
+                                .foregroundColor(.cyan)
+
+                    }
                 }.padding()
             }
-        .navigationTitle("Tips")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                } label: {
-                    Label("Add food", systemImage: "plus.circle")
+            .navigationTitle("Tips")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                    } label: {
+                        Label("Add food", systemImage: "plus.circle")
+                    }
                 }
-            }
-            ToolbarItem(placement: .navigationBarLeading) {
-                EditButton()
-            }
-        }//toolbar
-        
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }//toolbar
+            
         }
     }
 }
